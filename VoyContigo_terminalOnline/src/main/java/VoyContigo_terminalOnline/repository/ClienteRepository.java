e451fb8457b3c0ad089edb10ab812a97455e458a
BinCodeStudios <elklul182021@gmail.com>
2025-05-20 12:34:29 -0500

    Delete VoyContigo_terminalOnline directory

--- VoyContigo_terminalOnline/src/main/java/VoyContigo_terminalOnline/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package VoyContigo_terminalOnline.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import VoyContigo_terminalOnline.entity.Cliente;
-
-public interface ClienteRepository extends MongoRepository<Cliente, String> {
-    Cliente findByCorreo(String correo);
-}
--- VoyContigo_terminalOnline/src/main/java/VoyContigo_terminalOnline/repository/OrigenDestinoRepository.java
+++
@@ -1,7 +0,0 @@
-package VoyContigo_terminalOnline.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import VoyContigo_terminalOnline.entity.OrigenDestino;
-
-public interface OrigenDestinoRepository extends MongoRepository<OrigenDestino, String> {
-}
